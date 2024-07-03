b38bc799fe130fdc3782a5492626f478f92adaa1
Babahasko <zakutni4ek@gmail.com>
2024-07-03 23:53:16 +0300

    Add registration

--- authController.js
+++ authController.js
@@ -1,12 +1,30 @@
-const User = require("./models/User");
-const Role = require("./models/Role");
+const User = require('./models/User');
+const Role = require('./models/Role');
+const {logger} = require('./logger.js')
+const bcrypt = require('bcryptjs')
+const {validationResult} = require('express-validator')
 
 class authController {
     async registration(req, res) {
         try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({message: 'Ошибка при регистрации', errors})
+            }
+            const {username, password} = req.body
+            const candidate = await User.findOne({username})
+            if (candidate) {
+                return res.status(400).json({message: 'Пользователь с таким именем уже существует'})
+            }
+            const hashPassword = bcrypt.hashSync(password, 7);
+            const userRole = await Role.findOne({value: 'user'});
 
+            const user = new User({username, password: hashPassword, roles: [userRole.value]});
+            await user.save();
+            return res.json({message: 'Пользователь успешно зарегистрирован'});
         } catch (e) {
-
+            logger.error(e)
+            res.status(400).json({message: 'Registration error'})
         }
 
     }
@@ -22,10 +40,6 @@ class authController {
 
     async getUsers(req, res) {
         try {
-            const userRole = new Role()
-            const adminRole = new Role({value: "admin"})
-            await userRole.save()
-            await adminRole.save()
             res.json('server works')
         } catch (e) {
 
